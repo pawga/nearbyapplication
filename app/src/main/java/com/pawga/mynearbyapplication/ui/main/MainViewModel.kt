1153a30284c5cad1526f16c882fd836261631962
Pawga&Co <pawga777@gmail.com>
2021-02-11 00:24:50 +0300

    debug databinding

--- app/src/main/java/com/pawga/mynearbyapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pawga/mynearbyapplication/ui/main/MainViewModel.kt
@@ -1,16 +1,12 @@
 package com.pawga.mynearbyapplication.ui.main
 
 import android.view.View
-import androidx.annotation.IdRes
-import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.pawga.mynearbyapplication.R
 import com.pawga.mynearbyapplication.domain.State
-import com.pawga.mynearbyapplication.extensions.exhaustive
-import timber.log.Timber
 
 class MainViewModel : ViewModel() {
 
@@ -21,12 +17,12 @@ class MainViewModel : ViewModel() {
         _stateLiveData.value = state
     }
 
-    fun isEnable(view: View, status: State): Boolean {
-        return when (view.id) {
-            R.id.find_opponents -> { status == State.RequiredOpponent }
-            R.id.disconnect, R.id.message, R.id.send -> { status == State.Ready }
-            else -> true
-        }
+    fun findOpponents() {
+        _stateLiveData.value = State.Ready
+    }
+
+    fun disconnect() {
+        _stateLiveData.value = State.RequiredOpponent
     }
 
     class Factory() : ViewModelProvider.NewInstanceFactory() {
